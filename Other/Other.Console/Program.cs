c6c452943fe4fdcd3a9d20cfe9514eda5c03b24f
Max <u.kaihatsu@outlook.com>
2022-10-10 22:59:06 +0500

    Added other projects

---
+++ Other/Other.Console/Program.cs
@@ -0,0 +1,8 @@
+﻿using Other.JWT;
+// See https://aka.ms/new-console-template for more information
+
+TokenGenerator generatorJWT = new TokenGenerator();
+string jwtToken = generatorJWT.Authenticate("Paper");
+
+Console.WriteLine(jwtToken);
+Console.ReadKey();
---
+++ Other/Other.JWT/TokenGenerator.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace Other.JWT;
+
+public class TokenGenerator
+{
+    private const string SecretCode = "УБелкиПушистыйХвост";
+
+    public string Authenticate(string user)
+    {
+        if (string.IsNullOrWhiteSpace(user))
+        {
+            return string.Empty;
+        }
+
+        return GenerateJwtToken(user);
+    }
+
+    private string GenerateJwtToken(string nameIdentifier)
+    {
+        JwtSecurityTokenHandler jwtSecurityTokenHandler = new JwtSecurityTokenHandler();
+        byte[] key = Encoding.ASCII.GetBytes(SecretCode);
+
+
+        SecurityTokenDescriptor securityTokenDescriptor = new SecurityTokenDescriptor();
+        securityTokenDescriptor.Expires = DateTime.UtcNow.AddMinutes(15);
+        securityTokenDescriptor.Issuer = "Issuer";
+        //securityTokenDescriptor.Claims = "Issuer";
+
+        securityTokenDescriptor.SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature);
+        securityTokenDescriptor.Subject = new ClaimsIdentity(new Claim[] {
+                new Claim(ClaimTypes.NameIdentifier, nameIdentifier),
+                new Claim("SecretType","SecretValue")
+            });
+
+        SecurityToken securityToken = jwtSecurityTokenHandler.CreateToken(securityTokenDescriptor);
+        return jwtSecurityTokenHandler.WriteToken(securityToken);
+    }
+}
